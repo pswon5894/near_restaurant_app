30ff989d2e37f862f63fcb91d862bf802592f1b9
pswon5894 <pswon5894@gmail.com>
2025-11-19 21:03:26 +0900

    Initial commit

---
+++ app/src/main/java/com/cc/near_resaurant_app/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.cc.near_resaurant_app
+
+import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cc.near_resaurant_app.databinding.ActivityMainBinding
+import java.io.IOException
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    lateinit var locationProvider: LocationProvider
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    val REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    lateinit var getGPSPermissionLauncher : ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding =ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        checkAllPermissions()
+        updateUI()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun checkAllPermissions(){
+        if(!isLocationServicesAvailable()){
+            showDialogForLocationServiceSetting()
+        } else{
+            isRunTimePermissionGranted()
+        }
+    }
+
+    private fun isLocationServicesAvailable() : Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER))
+    }
+
+    private fun isRunTimePermissionGranted() {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(this@MainActivity,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this@MainActivity,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        if(hasFineLocationPermission != PackageManager.PERMISSION_GRANTED || hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@MainActivity,REQUIRED_PERMISSIONS,PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    private fun showDialogForLocationServiceSetting() {
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {result ->
+            if(result.resultCode == Activity.RESULT_OK) {
+                if(isLocationServicesAvailable()) {
+                    isRunTimePermissionGranted()
+                } else {
+                    Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 꺼져있습니다. 설정해야 앱을 사용할 수 있습니다.")
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialogInterface, i ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingIntent)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener {dialogInterface, i ->
+            dialogInterface.cancel()
+            Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+            finish()
+        })
+        builder.create().show()
+    }
+
+    private fun updateUI() {
+        locationProvider = LocationProvider(this@MainActivity)
+
+        val latitude: Double? = locationProvider.getLocationLatitude()
+        val longitude : Double? = locationProvider.getLocationLongitude()
+
+        if(latitude != null && longitude != null) {
+            //1. 현재 위치 가져오고 UI 업데이트
+            val address = getCurrentAddress(latitude, longitude)
+
+            address?.let{
+                binding.tvLocationTitle.text="${it.subLocality}"
+                //도로명
+                binding.tvLocationSub.text="${it.countryName} ${it.adminArea}"
+            }
+        }else{
+            Toast.makeText(this, "위도, 경도 정보를 가져올 수 없습니다.", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun getCurrentAddress (latitude : Double, longitude : Double) : Address?{
+        val geoCoder = Geocoder(this, Locale.KOREA)
+        //getDefault() or KOREA
+        val addresses: List<Address>?
+
+        addresses = try {
+            geoCoder.getFromLocation(latitude, longitude, 7)
+        }catch (ioException : IOException){
+            Toast.makeText(this, "지오코더 서비스를 이용불가 합니다", Toast.LENGTH_LONG).show()
+            return null
+        }catch (illegalArgumentException : java.lang.IllegalArgumentException){
+            Toast.makeText(this, "잘못된 위도, 경도입니다.", Toast.LENGTH_LONG).show()
+            return null
+        }
+
+        if(addresses == null || addresses.size == 0) {
+            Toast.makeText(this, "주소가 발견되지 않았습니다.", Toast.LENGTH_LONG).show()
+            return null
+        }
+
+        return addresses[0]
+    }
+}
+
+//ContextCompat은 Resource에서 값을 가져오거나 퍼미션을 확인할 때 사용할 때 SDK버전을 고려하지 않아도 되도록 (내부적으로 SDK버전을 처리해둔) 클래스
